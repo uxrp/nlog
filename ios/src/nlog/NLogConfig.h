35f163acf222101b0c109dedd68a87a9afd2c10b
Miller <chen.minliang@gmail.com>
2013-07-16 19:26:10 +0800

    modify receiverUrl to online

--- ios/src/nlog/NLogConfig.h
+++ ios/src/nlog/NLogConfig.h
@@ -11,7 +11,8 @@
 
 
 // 服务器日志接收路径
-#define RECEIVER_URL @"http://hunter.duapp.com/command/?command=nlog-post&channel=miller"
+//#define RECEIVER_URL @"http://hunter.duapp.com/command/?command=nlog-post&channel=miller"
+#define RECEIVER_URL @"http://kstj.baidu.com/ctj/88/?command=nlog-post"
 
 // 版本
 #define SDK_VERSION @"1.0"
